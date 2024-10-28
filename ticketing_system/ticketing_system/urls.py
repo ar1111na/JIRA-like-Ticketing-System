315bd7283f257c4b42cf41db4d6a84789f713fa3
abdulayef1 <7ka1cyr@code.edu.az>
2024-10-28 15:10:54 +0100

    homepage and layout added

--- ticketing_system/ticketing_system/urls.py
+++ ticketing_system/ticketing_system/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
+    path('', views.homepage),
     #path('users/', include('users.urls')),
     path('api/', include('tickets.urls')),  # Include API routes
     path('tickets/', include('tickets.urls')),   # Include HTML routes for ticket management
