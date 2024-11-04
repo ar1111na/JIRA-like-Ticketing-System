935d8fe3566ee06e523882084575b34ade41fe3b
abdulayef1 <7ka1cyr@code.edu.az>
2024-11-04 03:02:30 +0100

    register view fixed

--- ticketing_system/users/views.py
+++ ticketing_system/users/views.py
@@ -16,7 +16,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)  # Log in the user after registration
-            return redirect("/")
+            return redirect("/users/profile/")
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {"form": form})
